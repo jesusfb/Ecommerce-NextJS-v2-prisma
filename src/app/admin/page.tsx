6f0a42d08bfd1f5f1015a157a8650655230067eb
Rodrigo <rod_novaes@hotmail.com>
2024-09-19 21:17:15 +0200

    Add Cards ui elements

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function AdminDashboard() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <DashboardCard title='Sales' subtitle='Test' body='body' />
+      <DashboardCard title='Sales' subtitle='Test' body='body' />
+    </div>
+  );
+}
+
+type DasboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DasboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
+}
